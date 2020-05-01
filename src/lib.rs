9a8a289006e12ddd2b485b460e2c78a89a0cf56e
Mattia Rossini <mattia.rossini@en-ergo.it>
2020-05-01 17:36:22 +0200

    added sha1

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod easy_hasher {
     type _Byte = u8;
     type _Data = Vec<_Byte>;
     use sha2::Digest;
+    use sha1::Sha1;
 
     /// Performs the conversion from `u8` slice to `String`
     pub fn hex_string(data: _Data) -> String {
@@ -13,36 +14,51 @@ pub mod easy_hasher {
     }
 
     /// Generic hashing function
-    fn sha<T>(mut hasher: T, data: _Data) -> _Data where T: Digest {
+    fn sha2<T>(mut hasher: T, data: _Data) -> _Data where T: Digest {
         hasher.input(data.as_slice());
         hasher
             .result()
             .to_vec()
     }
 
+    fn sha(data: _Data) -> _Data {
+        let mut hasher = Sha1::new();
+        hasher.update(data.as_slice());
+        hasher.digest().bytes().to_vec()
+    }
+
     /* Raw data hashing functions */
+
+    /// SHA-1 raw data hashing function
+    pub fn sha1(d: _Data) -> _Data {
+        sha(d)
+    }
 
     /// SHA-224 raw data hashing function
     pub fn sha224(d: _Data) -> _Data {
-        sha(sha2::Sha224::new(), d)
+        sha2(sha2::Sha224::new(), d)
     }
 
     /// SHA-256 raw data hashing function
     pub fn sha256(d: _Data) -> _Data {
-        sha(sha2::Sha256::new(), d)
+        sha2(sha2::Sha256::new(), d)
     }
 
     /// SHA-384 raw data hashing function
     pub fn sha384(d: _Data) -> _Data {
-        sha(sha2::Sha384::new(), d)
+        sha2(sha2::Sha384::new(), d)
     }
 
     /// SHA-512 raw data hashing function
     pub fn sha512(d: _Data) -> _Data {
-        sha(sha2::Sha512::new(), d)
+        sha2(sha2::Sha512::new(), d)
     }
 
     /* String hashing functions */
+
+    pub fn string_sha1(s: String) -> _Data {
+        sha1(s.as_bytes().to_vec())
+    }
 
     /// SHA-224 string hashing function
     pub fn string_sha224(s: String) -> _Data {
