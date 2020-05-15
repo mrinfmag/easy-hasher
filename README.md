fc95fcd92a09bb20811118b4da2571ffff9b12c3
Mattia Rossini <mattia.rossini@en-ergo.it>
2020-05-15 19:30:13 +0200

    v1.0.0-rc: added Hash object

--- README.md
+++ README.md
@@ -9,18 +9,23 @@ Hashing functions (example with SHA256):
 <br/>
 Supported hashing algorithms: 
 
-* CRC8
-* CRC16
-* CRC32
-* CRC64
+* CRC
+  - CRC-8
+  - CRC-16
+  - CRC-32
+  - CRC-64
 * MD5
 * SHA1
 * SHA2
-  - SHA224
-  - SHA256
-  - SHA384
-  - SHA512
+  - SHA-224
+  - SHA-256
+  - SHA-384
+  - SHA-512
 * SHA3 
+  - SHA3-224
+  - SHA3-256
+  - SHA3-384
+  - SHA3-512
 
 File hashing support is coming soon too.
 
@@ -31,11 +36,12 @@ extern crate easy_hasher;
 use easy_hasher::easy_hasher::*;
 
 fn main() {
-	let string = "example string".to_string();
-	let hash = string_sha256(&string);
+    let string = "example string".to_string();
+    let hash = string_sha256(&string);
+    let string_hash = hash.to_hex_string();
 
-    assert_eq!(hash[..],
-               hex_literal::hex!("aedfb92b3053a21a114f4f301a02a3c6ad5dff504d124dc2cee6117623eec706")[..]);
-	println!("sha256({}) = {}", string, hex_string(hash));
+    assert_eq!(string_hash,
+               "aedfb92b3053a21a114f4f301a02a3c6ad5dff504d124dc2cee6117623eec706");
+    println!("sha256({}) = {}", string, string_hash);
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,31 @@ pub mod easy_hasher {
     type _Data = Vec<_Byte>;
     type _Input<'a> = &'a String;
 
+
+    pub struct Hash {
+        hash: _Data
+    }
+
+    impl Hash {
+        /// Performs the conversion from `Vec<u8>` to `String`
+        pub fn hex_string(data: &_Data) -> String {
+            data
+                .iter()
+                .map(|byte| format!("{:02x}", byte))
+                .collect()
+        }
+
+        /// Express hash as hex string
+        pub fn to_hex_string(&self) -> String {
+            Hash::hex_string(&self.hash)
+        }
+
+        /// Get hash as `Vec<u8>`
+        pub fn to_vec(&self) -> _Data {
+            self.hash.clone()
+        }
+    }
+
     use sha3::Digest;
     use sha1::Sha1;
 
@@ -15,24 +40,29 @@ pub mod easy_hasher {
             .collect()
     }
 
-    /// Generic hashing function
-    fn sha3<T>(mut hasher: T, data: _Data) -> _Data where T: Digest {
+    /// Generic SHA2/SHA3 hashing function
+    fn sha3<T>(mut hasher: T, data: _Data) -> Hash where T: Digest {
         hasher.input(data.as_slice());
-        hasher
-            .result()
-            .to_vec()
+        Hash {
+            hash: hasher
+                .result()
+                .to_vec()
+        }
     }
 
-    fn sha(data: _Data) -> _Data {
+    fn sha(data: _Data) -> Hash {
         let mut hasher = Sha1::new();
         hasher.update(data.as_slice());
-        hasher.digest().bytes().to_vec()
+        Hash {
+            hash: hasher.digest().bytes().to_vec()
+        }
     }
-
     /// CRC8 raw data hashing function, based on polynomial and initial value
-    pub fn param_crc8(data: _Data, poly: u8, init: u8) -> _Data {
+    pub fn param_crc8(data: _Data, poly: u8, init: u8) -> Hash {
         let mut crc8 = crc8::Crc8::create_msb(poly);
-        vec![crc8.calc(data.as_slice(), data.len() as i32, init)]
+        Hash {
+            hash: vec![crc8.calc(data.as_slice(), data.len() as i32, init)]
+        }
     }
 
     /* Raw data hashing functions */
@@ -41,80 +71,88 @@ pub mod easy_hasher {
 
     /// CRC8 raw data hashing function\
     /// poly = 0x07, init = 0x00
-    pub fn crc8(d: _Data) -> _Data {
+    pub fn crc8(d: _Data) -> Hash {
         param_crc8(d, 0x7, 0x0)
     }
 
     /// CRC16/ARC raw data hashing function
-    pub fn crc16(d: _Data) -> _Data {
-        crc16::State::<crc16::ARC>::calculate(d.as_slice()).to_be_bytes().to_vec()
+    pub fn crc16(d: _Data) -> Hash {
+        Hash {
+            hash: crc16::State::<crc16::ARC>::calculate(d.as_slice()).to_be_bytes().to_vec()
+        }
     }
 
     /// CRC32 raw data hashing function
-    pub fn crc32(d: _Data) -> _Data {
-        crc::crc32::checksum_ieee(d.as_slice()).to_be_bytes().to_vec()
+    pub fn crc32(d: _Data) -> Hash {
+        Hash {
+            hash: crc::crc32::checksum_ieee(d.as_slice()).to_be_bytes().to_vec()
+        }
     }
 
     /// CRC64 raw data hashing function
-    pub fn crc64(d: _Data) -> _Data {
-        crc::crc64::checksum_ecma(d.as_slice()).to_be_bytes().to_vec()
+    pub fn crc64(d: _Data) -> Hash {
+        Hash {
+            hash: crc::crc64::checksum_ecma(d.as_slice()).to_be_bytes().to_vec()
+        }
     }
 
     // MD5
 
     /// MD5 raw data hashing function
-    pub fn md5(d: _Data) -> _Data {
-        md5::compute(d).0.to_vec()
+    pub fn md5(d: _Data) -> Hash {
+        Hash {
+            hash: md5::compute(d).0.to_vec()
+        }
     }
 
     // SHA1
 
-    /// SHA-1 raw data hashing function
-    pub fn sha1(d: _Data) -> _Data {
+    /// SHA1 raw data hashing function
+    pub fn sha1(d: _Data) -> Hash {
         sha(d)
     }
 
     // SHA2
 
     /// SHA-224 raw data hashing function
-    pub fn sha224(d: _Data) -> _Data {
+    pub fn sha224(d: _Data) -> Hash {
         sha3(sha2::Sha224::new(), d)
     }
 
     /// SHA-256 raw data hashing function
-    pub fn sha256(d: _Data) -> _Data {
+    pub fn sha256(d: _Data) -> Hash {
         sha3(sha2::Sha256::new(), d)
     }
 
     /// SHA-384 raw data hashing function
-    pub fn sha384(d: _Data) -> _Data {
+    pub fn sha384(d: _Data) -> Hash {
         sha3(sha2::Sha384::new(), d)
     }
 
     /// SHA-512 raw data hashing function
-    pub fn sha512(d: _Data) -> _Data {
+    pub fn sha512(d: _Data) -> Hash {
         sha3(sha2::Sha512::new(), d)
     }
 
     // SHA3
 
     /// SHA3-224 raw data hashing function
-    pub fn sha3_224(d: _Data) -> _Data {
+    pub fn sha3_224(d: _Data) -> Hash {
         sha3(sha3::Sha3_224::new(), d)
     }
 
     /// SHA3-256 raw data hashing function
-    pub fn sha3_256(d: _Data) -> _Data {
+    pub fn sha3_256(d: _Data) -> Hash {
         sha3(sha3::Sha3_256::new(), d)
     }
 
     /// SHA3-384 raw data hashing function
-    pub fn sha3_384(d: _Data) -> _Data {
+    pub fn sha3_384(d: _Data) -> Hash {
         sha3(sha3::Sha3_384::new(), d)
     }
 
     /// SHA3-512 raw data hashing function
-    pub fn sha3_512(d: _Data) -> _Data {
+    pub fn sha3_512(d: _Data) -> Hash {
         sha3(sha3::Sha3_512::new(), d)
     }
 
@@ -124,80 +162,80 @@ pub mod easy_hasher {
 
     /// CRC8 string hashing function\
     /// poly = 0x07, init = 0x00
-    pub fn string_crc8(s: _Input) -> _Data {
+    pub fn string_crc8(s: _Input) -> Hash {
         crc8(s.clone().into_bytes())
     }
 
     /// CRC16/ARC string hashing function
-    pub fn string_crc16(s: _Input) -> _Data {
+    pub fn string_crc16(s: _Input) -> Hash {
         crc16(s.clone().into_bytes())
     }
 
     /// CRC32 string hashing function
-    pub fn string_crc32(s: _Input) -> _Data {
+    pub fn string_crc32(s: _Input) -> Hash {
         crc32(s.clone().into_bytes())
     }
 
     /// CRC64 string hashing function
-    pub fn string_crc64(s: _Input) -> _Data {
+    pub fn string_crc64(s: _Input) -> Hash {
         crc64(s.clone().into_bytes())
     }
 
     // MD5
 
     /// MD5 string hashing function
-    pub fn string_md5(s: _Input) -> _Data {
+    pub fn string_md5(s: _Input) -> Hash {
         md5(s.clone().into_bytes())
     }
 
     // SHA1
 
-    /// SHA-1 string hashing function
-    pub fn string_sha1(s: _Input) -> _Data {
+    /// SHA1 string hashing function
+    pub fn string_sha1(s: _Input) -> Hash {
         sha1(s.clone().into_bytes())
     }
 
     // SHA2
 
     /// SHA-224 string hashing function
-    pub fn string_sha224(s: _Input) -> _Data {
+    pub fn string_sha224(s: _Input) -> Hash {
         sha224(s.clone().into_bytes())
     }
 
     /// SHA-256 string hashing function
-    pub fn string_sha256(s: _Input) -> _Data {
+    pub fn string_sha256(s: _Input) -> Hash {
         sha256(s.clone().into_bytes())
     }
 
     /// SHA-384 string hashing function
-    pub fn string_sha384(s: _Input) -> _Data {
+    pub fn string_sha384(s: _Input) -> Hash {
         sha384(s.clone().into_bytes())
     }
 
     /// SHA-512 string hashing function
-    pub fn string_sha512(s: _Input) -> _Data {
+    pub fn string_sha512(s: _Input) -> Hash {
         sha512(s.clone().into_bytes())
     }
 
     // SHA3
 
     /// SHA3-224 string hashing function
-    pub fn string_sha3_224(s: _Input) -> _Data {
+    pub fn string_sha3_224(s: _Input) -> Hash {
         sha3_224(s.clone().into_bytes())
     }
 
     /// SHA3-256 string hashing function
-    pub fn string_sha3_256(s: _Input) -> _Data {
+    pub fn string_sha3_256(s: _Input) -> Hash {
         sha3_256(s.clone().into_bytes())
     }
 
     /// SHA3-384 string hashing function
-    pub fn string_sha3_384(s: _Input) -> _Data {
+    pub fn string_sha3_384(s: _Input) -> Hash {
         sha3_384(s.clone().into_bytes())
     }
 
     /// SHA3-512 string hashing function
-    pub fn string_sha3_512(s: _Input) -> _Data {
+    pub fn string_sha3_512(s: _Input) -> Hash {
         sha3_512(s.clone().into_bytes())
     }
 }
